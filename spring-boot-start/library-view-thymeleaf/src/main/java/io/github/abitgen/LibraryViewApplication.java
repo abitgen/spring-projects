b7c825df68930ae50caef10b071d4452575a91a0
abitgen <abitgenius@gmail.com>
2020-04-09 01:30:09 +0530

    Add Thymeleaf view as a new module for library application

---
+++ spring-boot-start/library-view-thymeleaf/src/main/java/io/github/abitgen/LibraryViewApplication.java
@@ -0,0 +1,14 @@
+package io.github.abitgen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryViewApplication {
+
+	public static void main(String[] args) {
+
+	    SpringApplication.run(LibraryViewApplication.class, args);
+	}
+
+}
---
+++ spring-boot-start/library-view-thymeleaf/src/main/java/io/github/abitgen/controller/ui/BookUISample.java
@@ -0,0 +1,13 @@
+package io.github.abitgen.controller.ui;
+
+/*import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BookUISample {
+
+    @RequestMapping("/sample")
+    public String getBookView(){
+        return "books";
+    }
+}*/
